2decd891d1a35a67fa7d58ac6773c770ce498bbc
HuydDo <huydinhdo@yahoo.com>
2019-11-20 10:26:25 -0700

    Done.

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe SessionsController, type: :controller do
   render_views
   describe 'post create' do
+
     it 'redirects to the login page if :name is nil' do
       post :create
       expect(response).to redirect_to controller: 'sessions', action: 'new'
